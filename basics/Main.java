766b06063c13f171b3b2532a9b57c6c1b9fbe5b6
shubham chauhan <64500580+shubhamppl@users.noreply.github.com>
2021-11-15 20:58:29 +0530

    Add files via upload

---
+++ basics/Main.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	int marks=59;
+	if(marks >75)
+	{
+	    System.out.println("a");
+	}
+	else if (marks>50)
+	{
+	 System.out.println("b");
+	}
+	}
+}
