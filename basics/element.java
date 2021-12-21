4d072b4aba10f1827d495d5698c88665e969034f
shubham chauhan <64500580+shubhamppl@users.noreply.github.com>
2021-12-21 12:48:24 +0530

    code for finding most frequent element

---
+++ basics/element.java
@@ -0,0 +1,17 @@
+class Main{
+     
+    Public static void Main(String[]args){
+    
+    
+    int[] arr= {1,2,3,4,5,3,4,4,4,2,1};
+    int i,j,count =0;element =0;
+    int count=0;
+    for(i=0;i< arr.lenght;i++)
+    int tempElement=arr[i];
+    int tempCount=0;
+    for(int j=0;j<arr.lenght;j++)
+     if (arr[j]==tempElement)
+     tempCount++;
+    
+    }
+}
